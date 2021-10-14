4538843d0b17f0c2ae444fbc53bb997c815006b3
Nima sherpa <Toplouie78@gmail.com>
2021-10-14 11:24:49 -0400

    added the list of title to a select

--- script.js
+++ script.js
@@ -4,4 +4,14 @@ fetch('https://ghibliapi.herokuapp.com/films')
 .then(res => res.json())
 .then(data => {
     console.log(data);
+    for (let d of data){
+
+        let select = document.querySelector("select");
+        let newOption = document.createElement("option");
+        let { title } = d;
+        newOption.textContent = title[0].toUpperCase() + title.slice(1);
+        newOption.value = title ;
+        select.append(newOption);
+
+    }
 })
