82cbbc42522d86f736b91e0bb2635e2ee643ccbc
Nima sherpa <Toplouie78@gmail.com>
2021-10-14 14:35:11 -0400

    completed form functionality

--- script.js
+++ script.js
@@ -2,25 +2,27 @@ fetch('https://ghibliapi.herokuapp.com/films')
 .then(res => res.json())
 .then(data => {
     console.log(data);
+    // recive the data from api
+
     for (let d of data){
         let select = document.querySelector("select");
         let newOption = document.createElement("option");
         newOption.classList.add(".title_option")
         let { title } = d;
-        // newOption.textContent = title[0].toUpperCase() + title.slice(1);
         newOption.textContent = title;
         newOption.value = title ;
         select.append(newOption);
     }
 
     let select = document.querySelector("select");
+    let title_main = document.createElement("h3");
+    let year_p = document.createElement("p");
+    let disc_p = document.createElement("p");
+
+    // changing the description based on which title it has been clicked.
 
     select.addEventListener("change", (e) => {
-        let title_main = document.createElement("h3");
-        let year_p = document.createElement("p");
-        let disc_p = document.createElement("p");
         let display = document.querySelector("#display-info");
-
         display.append(title_main, year_p , disc_p);
          for (let i= 0; i < data.length; i++ ) {
             let title = data[i].title;
@@ -32,7 +34,30 @@ fetch('https://ghibliapi.herokuapp.com/films')
                 disc_p.textContent = description;
                 year_p.textContent = release_date ;
             }
-            
         }
     })
+
+    //creating the review format
+
+    let form = document.querySelector("form");
+   form.addEventListener("submit", (e) => {
+    let titles = document.querySelector('#selected_title').value;
+    e.preventDefault();
+
+    document.submit_form.reset();
+
+    let ul = document.querySelector("ul");
+    let li = document.createElement("li");
+    // let select_input = document.querySelector()
+
+    li.innerHTML = `
+    Hey how are you 
+    <span>${titles}</span>
+    `
+    ul.append(li);
+
+   } )
 })
+.catch(err => {
+    console.log("err")
+});
